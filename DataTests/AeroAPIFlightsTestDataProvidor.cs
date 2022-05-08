0ba43db580615493e6ec1e55ca2d34cea0b354fb
Michael Freudeman <michael.freudeman@gmail.com>
2022-05-07 23:16:22 -0400

    Add LFS.Data namespace tests project

---
+++ DataTests/AeroAPIFlightsTestDataProvidor.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Reflection;
+using LFS.Data.API.AeroAPI;
+
+namespace DataTests
+{
+    internal class AeroAPIFlightsTestDataProvidor : ITestDataProvidor
+    {
+        public Stream Response()
+        {
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            string directoryPath = Path.GetDirectoryName(assemblyPath);
+            string dataFilePath = Path.Combine(new string[] { directoryPath, "DataFiles", "AeroAPI", "flights_jbu906.json" });
+            if (!File.Exists(dataFilePath))
+            {
+                throw new FileNotFoundException("Unable to find file", dataFilePath);
+            }
+            return File.OpenText(dataFilePath).BaseStream;
+        }
+    }
+}
---
+++ DataTests/AeroAPITests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LFS.Data.API.AeroAPI;
+using LFS.Data.API.AeroAPI.Model;
+using LFS.Data.API.AeroAPI.Helper;
+
+namespace DataTests
+{
+    [TestClass]
+    public class AeroAPITests
+    {
+        [TestMethod]
+        public void TestFlightsByFlightIdentifier()
+        {
+            AeroAPI.TestDataProvidor = new AeroAPIFlightsTestDataProvidor();
+            
+            var response = AeroAPI.FlightsByFlightIdentifier("apikeyvalue", "JBU906");
+            response.Wait();
+            var result = response.Result;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Results);
+            Assert.IsTrue(result.Results.Count > 0);
+
+            Flight inProgressFlight = FlightsHelper.FirstInProgress(result.Results);
+
+            Assert.IsNotNull(inProgressFlight);
+        }
+    }
+}
