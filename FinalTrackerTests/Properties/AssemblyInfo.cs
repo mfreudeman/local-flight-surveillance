64c61606536ddd9b13b536ba45140bb3441d546e
Michael Freudeman <michael.freudeman@gmail.com>
2022-05-11 21:34:31 -0400

    Add FinalTracker tests webapi test class

---
+++ FinalTrackerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FinalTrackerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FinalTrackerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88ba1a82-4e5d-442c-a103-714c3216c079")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FinalTrackerTests/WebAPITests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinalTracker.WebAPI;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+
+namespace FinalTrackerTests
+{
+    class TestDataContext
+    {
+        public string TestString { get; set; }
+        public int TestInt { get; set; }
+        public float TestFloat { get; set; }
+    }
+
+    [TestClass]
+    public class WebAPITests
+    {
+        [TestMethod]
+        public void TestWebServicesServer()
+        {
+            string testPath = "/test/path";
+            TestDataContext dataContext = new TestDataContext()
+            {
+                TestString = "TestTest",
+                TestInt = 42,
+                TestFloat = 3.14159f
+            };
+            JsonDataProvider<TestDataContext> dataProvider = new JsonDataProvider<TestDataContext>(dataContext);
+            HttpResponseMessage response;
+
+            using (WebServicesServer server = new WebServicesServer("localhost", 27100))
+            {
+                server.RegisterEndPoint(testPath, dataProvider);
+
+                Thread.Sleep(10); // Wait a bit for the server to be bound.
+
+                HttpClient client = new HttpClient();
+                var get = client.GetAsync("http://localhost:27100" + testPath);
+                get.Wait();
+                response = get.Result;
+            }
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual(dataProvider.ContentType, response.Content.Headers.ContentType.ToString());
+
+            var contentTask = response.Content.ReadAsStringAsync();
+            contentTask.Wait();
+            string content = contentTask.Result;
+            TestDataContext receivedData = JsonSerializer.Deserialize<TestDataContext>(content);
+
+            Assert.IsNotNull(receivedData);
+            Assert.AreEqual(dataContext.TestString, receivedData.TestString);
+            Assert.AreEqual(dataContext.TestInt, receivedData.TestInt);
+            Assert.AreEqual(dataContext.TestFloat, receivedData.TestFloat);
+        }
+    }
+}
